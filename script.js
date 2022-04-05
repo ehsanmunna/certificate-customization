076a3b6f5011db1bb51f6ccb8f4722b9ac13f1bc
ehsan munna <grmunnabd@gmail.com>
2022-04-05 13:51:55 +0600

    text and image added to layer

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-const { default: Konva } = require("konva");
+// const { default: Konva } = require("konva");
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const color = '#273746';
 
-var stage = new Konva.Stage({
-    container: 'container',
-    width: width,
-    height: height,
-    draggable: true,
+const stage = new Konva.Stage({
+  container: 'container',
+  width: width,
+  height: height,
+  draggable: true,
+});
+
+var simpleText = new Konva.Text({
+  x: stage.width() / 2,
+  y: 15,
+  text: 'Bangladesh Technical Education Board',
+  fontSize: 30,
+  fontFamily: 'Calibri',
+  fill: color,
+});
+simpleText.offsetX(simpleText.width() / 2);
+
+// signature image
+var imageObj = new Image();
+imageObj.onload = function () {
+  var signature1 = new Konva.Image({
+    x: 50,
+    y: height - 180,
+    image: imageObj,
+    width: 106,
+    height: 118,
   });
 
-var layer = new Konva.Layer();
-stage.add(layer);
+  // add the shape to the layer
+  layer.add(signature1);
+};
+imageObj.src = './assets/signature/sign-1.png';
 
-console.log(width, height)
+// footer line
+const startFromX = 0;
+const startFromY = 0;
+const widthLine = 290;
+
+
+var line = new Konva.Line({
+  x: 50,
+  y: height - 50,
+  points: [startFromX, startFromY, (startFromX + widthLine), startFromY],
+  stroke: color,
+  tension: 1
+});
+
+const layer = new Konva.Layer();
+layer.add(simpleText)
+layer.add(line);
+stage.add(layer);
+console.log(line)
